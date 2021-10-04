bea0f8beca4cba7f85fdca5a7950680fe4b5a08d
Cornelius-github <85209828+Cornelius-github@users.noreply.github.com>
2021-10-04 12:23:40 +1100

    Button Functionality
    Wrote code that turned the text based version into working with the button selection. This meant a new script in order to keep things clean and tidy. I then implemented it into the game scene under a new game object called Game Rules.

--- Coliseum/Assets/Scripts/TextBasedVersion.cs
+++ Coliseum/Assets/Scripts/TextBasedVersion.cs
@@ -275,4 +275,125 @@ public class TextBasedVersion : MonoBehaviour
         print("You have swapped to " + thePlayer[p]);
         
     }
+
+    //methods for the button presses
+    public void Attack()
+    {
+        EnemyChoice();
+        PlayerAttacks();
+    }
+
+    public void Defend()
+    {
+        EnemyChoice();
+        PlayerDefends();
+    }
+
+    //for swapping weapons
+    public void Sword()
+    {
+        p = thePlayer.IndexOf("sword");
+    }
+
+    public void Hammer()
+    {
+        p = thePlayer.IndexOf("hammer");
+    }
+
+    public void Spear()
+    {
+        p = thePlayer.IndexOf("spear");
+    }
+
+    public void PlayerAttacks()
+    {
+        int enemyAction = Random.Range(0, 1);
+        if (enemyAction == 0)
+        {
+            //both attack
+            print("Both sides chose to attack");
+            if (thePlayer[p] == theComputer[c])
+            {
+                //same weapon
+                print("Both sides tried to attack with " + thePlayer[p] + "s, resulting in a clash. Nothing was won");
+            }
+            if (thePlayer[p] == "sword" && theComputer[c] == "spear" || thePlayer[p] == "spear" && theComputer[c] == "hammer" || thePlayer[p] == "hammer" && theComputer[c] == "sword")
+            {
+                //player wins
+                glory = glory + 2;
+                print("You used a " + thePlayer[p] + ", while your opponent used a " + theComputer[c] + ". You won that round.");
+            }
+            if (theComputer[c] == "sword" && thePlayer[p] == "spear" || theComputer[c] == "spear" && thePlayer[p] == "hammer" || theComputer[c] == "hammer" && thePlayer[p] == "sword")
+            {
+                //player loses
+                glory = glory - 2;
+                print("You used a " + thePlayer[p] + ", while your opponent used a " + theComputer[c] + ". You lost that round.");
+            }
+        }
+        else
+        {
+            //enemy defends
+            //player attacks
+            print("You attacked while your opponent defended");
+
+            if (thePlayer[p] == theComputer[c])
+            {
+                //same weapon
+                glory = glory - 1;
+                print("Both sides used " + thePlayer[p] + "s, resulting in your opponent defended from your attack. You Lost");
+            }
+            if (thePlayer[p] == "sword" && theComputer[c] == "spear" || thePlayer[p] == "spear" && theComputer[c] == "hammer" || thePlayer[p] == "hammer" && theComputer[c] == "sword")
+            {
+                //player wins
+                glory = glory + 1;
+                //breaks opponent weapon
+                theComputer.RemoveAt(c);
+                print("You used a " + thePlayer[p] + ", while your opponent used a " + theComputer[c] + " which you managed to break. You won that round");
+            }
+            if (theComputer[c] == "sword" && thePlayer[p] == "spear" || theComputer[c] == "spear" && thePlayer[p] == "hammer" || theComputer[c] == "hammer" && thePlayer[p] == "sword")
+            {
+                //player loses
+                glory = glory - 1;
+                //breaks own weapon
+                thePlayer.RemoveAt(p);
+                print("You used a " + thePlayer[p] + ", while your opponent used a " + theComputer[c] + ". Although your weapon broke, you managed to lose");
+            }
+        }
+    }
+    public void PlayerDefends()
+    {
+        int enemyAction = Random.Range(0, 1);
+        if (enemyAction == 0)
+        {
+            //player defends
+            print("You defended from the opponent's attack");
+            if (thePlayer[p] == theComputer[c])
+            {
+                //same weapon
+                glory = glory - 1;
+                print("Both sides used " + thePlayer[p] + "s, resulting in a clash. Nothing was won");
+            }
+
+            if (thePlayer[p] == "sword" && theComputer[c] == "spear" || thePlayer[p] == "spear" && theComputer[c] == "hammer" || thePlayer[p] == "hammer" && theComputer[c] == "sword")
+            {
+                //player wins
+                glory = glory + 1;
+                //breaks opponent weapon
+                theComputer.RemoveAt(c);
+                print("You used a " + thePlayer[p] + ", while your opponent used a " + theComputer[c] + " which you managed to break. You won that round");
+            }
+
+            if (theComputer[c] == "sword" && thePlayer[p] == "spear" || theComputer[c] == "spear" && thePlayer[p] == "hammer" || theComputer[c] == "hammer" && thePlayer[p] == "sword")
+            {
+                //player loses
+                glory = glory - 2;
+                print("You used a " + thePlayer[p] + ", while your opponent used a " + theComputer[c] + ". You lost that round.");
+            }
+        }
+        else
+        {
+            //nothing happens
+        }
+        
+    }
 }
